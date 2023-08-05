fe3565678d2e5e8295cd7c5004dc9e7d3ae42c04
Nazar <140441511+NazarKarpa@users.noreply.github.com>
2023-08-05 22:36:03 +0300

    Add files via upload

--- Logika/m1/u2/Спадкування/main.py
+++ Logika/m1/u2/Спадкування/main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Widhet:
+    def __init__(self,text):
+        self.text = text
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+class Button(Widhet):
+    x = None
+    y = None
+    def click(self,text):
+        self.text = text
+d = Button("А жаль")
+b = Widhet("ви уже записаний")
+x = 100
+Button.x = 100
+Button.y = 100
+print("Хочете брати участь?")
+print("Розташовання",Button.x,"на",Button.y)
+a = input("Так-Не")
+
+if a == "Так":
+    print(b)
+else:
+    print(d)
+
