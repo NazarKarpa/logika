2f22de5c41bafc04cac5f8d42afbd371fbbd4052
Nazar <140441511+NazarKarpa@users.noreply.github.com>
2023-08-07 21:29:10 +0300

    Add files via upload

--- Logika/m1/u2/Спадкування/main.py
+++ Logika/m1/u2/Спадкування/main.py
@@ -6,30 +6,43 @@ class Widhet:
         self.y = y
 
 
-    def druk(self,metod):
-        metod = "123"
-        print("Напис",metod)
-        print('Розташування',x,y)
+    def druk(self):
+        print("Напис:",self.text)
+    def druk2(self):
+        print(self.text)
+    def druk3(self):
+        print(self.text)
+
+    def druk4(self):
+        print('Розташування',self.x,self.y)
 
 
 
 
 class Button(Widhet):
 
-    def click(self,click):
+    def click(self,):
+        dear.druk2()
         click = True
-        print("Ви записані")
+
+    def folse(self):
+        dar.druk3()
 
 
 
+dear = Button("Ви записані",100,100)
+dar = Button("а шкода",100,100)
+der = Widhet("розташовання",100,100)
+dwar = Widhet("Брати учать на шоколадку 10 річну?",100,100)
+dwar.druk()
 
-
-a = input("Так-Не")
+der.druk4()
+a = input("Хочете зареєструватися? Так-Не")
 
 if a == "Так":
-    click()
+    dear.click()
 
 else:
-    print("Мне пофек")
+    dar.folse()
 
 
