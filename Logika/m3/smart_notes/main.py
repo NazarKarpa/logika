d352b0cb7beb3f3854597e96e02716c4a1d8df8d
Nazar <140441511+NazarKarpa@users.noreply.github.com>
2023-09-25 19:29:19 +0300

    Add files via upload

--- Logika/m3/smart_notes/main.py
+++ Logika/m3/smart_notes/main.py
@@ -16,28 +16,51 @@ btn_note_create = QPushButton("Стровити замітку")
 btn_note_del = QPushButton("Видалити замітку")
 btn_note_save = QPushButton("Зберегти замітку")
 
-lb_tags = QLabel("Список заміток")
+lb_tags = QLabel("Список тегів")
 
 lst_tags = QListWidget()
 
 field_tag = QLineEdit()
-btn_tags_add = QPushButton("Додатиa Тег")
-btn_tgs_del = QPushButton("Видалити Тег")
-btn_tags_search = QPushButton("Шукати Тег")
+btn_tags_add = QPushButton("Додати до замітки")
+btn_tgs_del = QPushButton("Відкріпити від заміти")
+btn_tags_search = QPushButton("Шукати замітки за тегом")
+
+
 row1 = QHBoxLayout()
+
 row1.addWidget(btn_note_create)
 row1.addWidget(btn_note_del)
+
+row2 = QHBoxLayout()
+
+
 layout_notes = QHBoxLayout()
+
 col1 = QVBoxLayout()
 col2 = QVBoxLayout()
+
+col2.addWidget(lb_notes)
 col2.addWidget(lst_notes)
+
 layout_notes.addLayout(col1, stretch=2)
 layout_notes.addLayout(col2, stretch=1)
 
-col1.addWidget(field_text)
-col2.addLayout(row1)
+row2.addWidget(btn_tags_search)
+row2.addWidget(btn_tgs_del)
 
 col2.addWidget(btn_note_save)
+col2.addWidget(lb_notes)
+
+col1.addWidget(field_text)
+
+col2.addLayout(row1)
+col2.addWidget(lb_tags)
+col2.addWidget(lst_tags)
+col2.addWidget(field_tag)
+
+col2.addWidget(btn_tags_add)
+col2.addLayout(row2)
+
 def show_notes():
     key = lst_notes.selectedItems()[0].text()
     field_text.setText(notes[key]['текст'])
