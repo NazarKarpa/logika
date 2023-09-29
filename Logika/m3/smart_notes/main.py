8f1d4cf8542ac25dca550e759fc3473a5db09851
Nazar <140441511+NazarKarpa@users.noreply.github.com>
2023-09-29 20:03:09 +0300

    Add files via upload

--- Logika/m3/smart_notes/main.py
+++ Logika/m3/smart_notes/main.py
@@ -6,6 +6,12 @@ from PyQt5.QtWidgets import (
     QTableWidget,  QListWidgetItem, QFormLayout,
     QGroupBox, QButtonGroup, QRadioButton, QSpinBox)
 import json
+
+def writeToFile():
+    with open("notes.json", "w", encoding="utf8") as file:
+        json.dump(notes, file, ensure_ascii=False, sort_keys=True, indent=4)
+
+
 app = QApplication([])
 window = QWidget()
 
@@ -64,6 +70,44 @@ col2.addLayout(row2)
 def show_notes():
     key = lst_notes.selectedItems()[0].text()
     field_text.setText(notes[key]['текст'])
+
+    lst_tags.clear()
+    lst_tags.addItems(notes[key]["теги"])
+def add_note():
+    note_name, ok = QInputDialog.getText(window, "Додати замітку", "Назва замітки")
+    if note_name and ok:
+        lst_notes.addItem(note_name)
+        notes[note_name] = {"текст": "", "теги": []}
+        writeToFile()
+
+def del_note():
+    if lst_notes.currentItem():
+        key = lst_notes.currentItem().text()
+    key = lst_notes.currentItem().text()
+    del notes[key]
+    writeToFile()
+
+    field_text.clear()
+    lst_tags.clear()
+    lst_notes.clear()
+    lst_notes.addItems(notes)
+
+def save_note():
+    if lst_notes.currentItem():
+        key = lst_notes.currentItem().text()
+
+
+        notes[key]["текст"] = field_text.toPlainText()
+        writeToFile()
+
+btn_note_save.clicked.connect(save_note)
+
+btn_note_del.clicked.connect(del_note)
+
+btn_note_create.clicked.connect(add_note)
+
+
+
 lst_notes.itemClicked.connect(show_notes)
 with open("notes.json", "r", encoding="utf8") as file:
     notes = json.load(file)
