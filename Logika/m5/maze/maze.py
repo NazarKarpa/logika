2cc80c7b60ef37675cd640ea8011f78c53767927
Nazar <140441511+NazarKarpa@users.noreply.github.com>
2023-11-24 21:03:19 +0300

    Add files via upload

--- Logika/m5/maze/maze.py
+++ Logika/m5/maze/maze.py
@@ -19,7 +19,31 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+        if keys[K_d] and self.rect.x < win_width - 80:
+            self.rect.x += self.speed
+        if keys[K_a] and self.rect.x > 5:
+            self.rect.x -= self.speed
 
+
+class Enemy(GameSprite):
+    direction = "left"
+    def update(self):
+        if self.direction == "left":
+            self.rect.x -= self.speed
+        else:
+            self.rect.x += self.speed
+
+        if self.rect.x <= 500:
+            self.direction = 'right'
+        elif self.rect.x >= win_width - 80:
+            self.direction = "left"
 
 
 
@@ -31,12 +55,13 @@ window = display.set_mode((win_width, win_height))
 background = scale(load("background.jpg"), (win_width, win_height))
 
 
-player = GameSprite("hero.png", 5, win_height - 80, 5)
-gyborg = GameSprite("cyborg.png", win_width - 100, win_height - 300,  2)
-
+player = Player("hero.png", 5, win_height - 80, 5)
+gyborg = Enemy("cyborg.png", win_width - 100, win_height - 300,  2)
+final = GameSprite("treasure.png", win_width - 80, win_height - 80, 0)
 
 
 game = True
+finish = False
 clock = time.Clock()
 FPS = 60
 
@@ -51,9 +76,19 @@ while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-    window.blit(background, (0, 0))
-    player.reset()
-    gyborg.reset()
+
+    if not finish:
+
+        window.blit(background, (0, 0))
+
+        player.reset()
+        gyborg.reset()
+        final.reset()
+        
+        gyborg.update()
+        player.update()
+
+
     display.update()
 
     clock.tick(FPS)
