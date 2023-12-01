4d9921fa9628f0a0d3ba3f60b34d4fedbf2d5694
Nazar <140441511+NazarKarpa@users.noreply.github.com>
2023-12-01 21:00:05 +0300

    Add files via upload

--- Logika/m5/maze/maze.py
+++ Logika/m5/maze/maze.py
@@ -50,6 +50,24 @@ class Enemy(GameSprite):
         elif self.rect.x >= win_width - 80:
             self.direction = "left"
 
+class Wall(sprite.Sprite):
+    def __init__(self, wall_x, wall_y, wall_wight, wall_height):
+        super().__init__()
+        self.widht = wall_wight
+        self.height = wall_height
+
+        self.image = Surface((self.widht, self.height))
+        self.image.fill((1,250,0))
+
+        self.rect = self.image.get_rect()
+
+        self.rect.x = wall_x
+
+        self.rect.y = wall_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+
 
 
 win_width = 700
@@ -59,11 +77,13 @@ window = display.set_mode((win_width, win_height))
 
 background = scale(load("background.jpg"), (win_width, win_height))
 
-
+wall_player1 = Wall(110, 0, 300, 20)
+wall_player2 = Wall(400, 0, 20, 350)
+wall_player3 = Wall(200, 200, 20, 350)
 player = Player("hero.png", 5, win_height - 80, 5)
 gyborg = Enemy("cyborg.png", win_width - 100, win_height - 300,  2, 500)
 final = GameSprite("treasure.png", win_width - 80, win_height - 80, 0)
-gyborg2 = Enemy("cyborg.png", win_width - 150, win_height - 150,  2, 350)
+gyborg2 = Enemy("cyborg.png", win_width - 150, win_height - 150,  2, 450)
 
 
 game = True
@@ -71,13 +91,19 @@ finish = False
 clock = time.Clock()
 FPS = 60
 
+font.init()
+
+f = font.Font(None, 70)
+win = f.render("You Win", True, (255,215,0))
+lose = f.render("You lose", True, (255,0,0))
 
 mixer.init()
 mixer.music.load("jungles.ogg")
 
 mixer.music.play()
 
-
+money_sound = mixer.Sound("money.ogg")
+kick_sound = mixer.Sound("kick.ogg")
 while game:
     for e in event.get():
         if e.type == QUIT:
@@ -86,14 +112,27 @@ while game:
     if not finish:
 
         window.blit(background, (0, 0))
-
+        wall_player1.reset()
+        wall_player2.reset()
+        wall_player3.reset()
         player.reset()
         gyborg.reset()
         final.reset()
         gyborg2.reset()
+
         gyborg2.update()
         gyborg.update()
         player.update()
+
+        if sprite.collide_rect(player, final):
+            finish = True
+            window.blit(win,(200,200))
+            money_sound.play()
+        if sprite.collide_rect(player, gyborg) or sprite.collide_rect(player, wall_player1) or sprite.collide_rect(player, wall_player2) or sprite.collide_rect(player, wall_player3) or sprite.collide_rect(player, gyborg2):
+
+            finish = True
+            window.blit(lose,(200,200))
+            kick_sound.play()
 
 
     display.update()
